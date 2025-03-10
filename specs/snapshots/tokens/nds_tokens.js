d9ab7ec354f82ef1670b1a5f8ad24fd915df0b19
Haider Alshamma <haidera@nulogy.com>
2025-03-10 14:34:41 -0400

    test: update fixtures and snapshots

--- specs/snapshots/tokens/nds_tokens.js
+++ specs/snapshots/tokens/nds_tokens.js
@@ -9,27 +9,30 @@
 | Colors
 |-----------------------------------------------------------------------------
 */
-export const TABLET_COLORS_PRIMARY = "#ffffff";
-export const TABLET_COLORS_SECONDARY = "#000000";
-export const TABLET_COLORS_NESTED_TERTIARY = "#ff00ff";
-export const PHONE_COLORS_PRIMARY = "#ffffff";
-export const PHONE_COLORS_SECONDARY = "#000000";
-export const PHONE_COLORS_NESTED_TERTIARY = "#ff00ff";
 export const DESKTOP_COLORS_PRIMARY = "#ffffff";
 export const DESKTOP_COLORS_SECONDARY = "#000000";
 export const DESKTOP_COLORS_NESTED_TERTIARY = "#ff00ff";
+export const DESKTOP_COLORS_CAMEL_CASE_PROPERTY = "#ff00ff";
+export const TABLET_COLORS_PRIMARY = "#ffffff";
+export const TABLET_COLORS_SECONDARY = "#000000";
+export const TABLET_COLORS_NESTED_TERTIARY = "#ff00ff";
+export const TABLET_COLORS_CAMEL_CASE_PROPERTY = "#ff00ff";
+export const PHONE_COLORS_PRIMARY = "#ffffff";
+export const PHONE_COLORS_SECONDARY = "#000000";
+export const PHONE_COLORS_NESTED_TERTIARY = "#ff00ff";
+export const PHONE_COLORS_CAMEL_CASE_PROPERTY = "#ff00ff";
 
 /*
 |-----------------------------------------------------------------------------
 | Spacing
 |-----------------------------------------------------------------------------
 */
-export const TABLET_SPACING_SMALL = "5.6";
-export const TABLET_SPACING_MEDIUM = "11.2";
-export const TABLET_SPACING_LARGE = "16.8";
-export const PHONE_SPACING_SMALL = "3.6";
-export const PHONE_SPACING_MEDIUM = "7.2";
-export const PHONE_SPACING_LARGE = "10.8";
-export const DESKTOP_SPACING_SMALL = "4";
-export const DESKTOP_SPACING_MEDIUM = "8";
-export const DESKTOP_SPACING_LARGE = "12";
+export const DESKTOP_SPACING_SMALL = "4px";
+export const DESKTOP_SPACING_MEDIUM = "8px";
+export const DESKTOP_SPACING_LARGE = "12px";
+export const TABLET_SPACING_SMALL = "5.6px";
+export const TABLET_SPACING_MEDIUM = "11.2px";
+export const TABLET_SPACING_LARGE = "16.799999999999997px";
+export const PHONE_SPACING_SMALL = "3.6px";
+export const PHONE_SPACING_MEDIUM = "7.2px";
+export const PHONE_SPACING_LARGE = "10.8px";
