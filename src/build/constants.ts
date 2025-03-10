4fdeef83b4fc404fab0e9eb0c569843fffb47294
Haider Alshamma <haidera@nulogy.com>
2025-03-10 14:34:18 -0400

    refactor: reorder base units

--- src/build/constants.ts
+++ src/build/constants.ts
@@ -2,9 +2,9 @@ export type TokenValue = string | number | Record<string, any>
 export type Device = 'desktop' | 'tablet' | 'phone'
 
 export const DEVICE_BASE_UNITS: Record<Device, number> = {
+  desktop: 4,
   tablet: 5.6,
   phone: 3.6,
-  desktop: 4,
 } as const
 
 export const OUTPUT_PATH_NAME = 'output'
